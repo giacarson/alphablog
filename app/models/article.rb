29c1641e3a68cf2730f18c0b70248e021b9182db
Virginia Carson <vlcarson.42@gmail.com>
2016-01-08 04:27:04 -0800

    add categories model and tests

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ActiveRecord::Base
 	belongs_to :user
 	validates :user_id, presence: true
 	validates :title, presence: true, length: {minimum: 3, maximum: 50}
-	validates :description, presence: true, length: {minimum: 15}
+	validates :description, presence: true, length: {minimum: 15, maximum: 150}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ get 'login', to: 'sessions#new'
 post 'login', to: 'sessions#create'
 delete 'logout', to: 'sessions#destroy'
 
+resources :categories
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
